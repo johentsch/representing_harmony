6568ed74028a5476d462e8145c43bdf1a6288874
johentsch <johannes.hentschel@epfl.ch>
2022-09-19 21:45:16 +0200

    adds pitch.py and abstract.py with a simplistic point-vector model for pitches and intervals

---
+++ pitch.py
@@ -0,0 +1,17 @@
+from abstract import Point, Vector
+
+
+class PitchClass(Point):
+    pass
+
+
+class Pitch(Point):
+    pass
+
+
+class IntervalClass:
+    pass
+
+
+class Interval(Vector):
+    pass
